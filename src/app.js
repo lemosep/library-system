63410bb6c919767d68277a82ef74aee85bcecf5e
Eduardo Lemos <dudulemox@gmail.com>
2023-05-03 16:19:07 -0300

    added handlebars

--- src/app.js
+++ src/app.js
@@ -1,9 +1,14 @@
 import express from "express";
+import { ExpressHandlebars } from "express-handlebars";
 import { PrismaClient } from "@prisma/client";
 
 import routes from "./routes.js";
 
 const app = express();
+
+app.engine("handlebars", ExpressHandlebars({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
 const prisma = new PrismaClient();
 
 app.use(express.json());
